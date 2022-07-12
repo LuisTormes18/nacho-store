278d13ea5780bd1a315a26441c03eb7eb6f865fd
luis tormes <luisangeltormes@gmail.com>
2022-07-12 12:51:34 -0400

    refactor

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,6 +1,5 @@
 import CardCategory from "./cards/CardCategory";
 import CardProduct from "./cards/CardProduct";
-import RowCategory from "./rowCategory/RowCategory";
 import Cart from "./Cart/Cart";
 import LayoutContainer from "./Layout";
 import TotalCartValue from "./Cart/TotalCartValue";
@@ -9,7 +8,6 @@ import CartItem from "./Cart/CartItem";
 export {
   CardCategory,
   CardProduct,
-  RowCategory,
   Cart,
   LayoutContainer,
   TotalCartValue,
