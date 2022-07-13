a32e4393d0524d8055a43d1970840ae19fe6547c
luis tormes <luisangeltormes@gmail.com>
2022-07-13 16:28:14 -0400

    refactor

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instanceAxios = axios.create({
-  baseURL: "http://localhost:4000/api/",
+  // baseURL: "http://localhost:4000/api/",
+  baseURL: "https://api-nacho-store.vercel.app/api/",
 });
 
 export default instanceAxios;
