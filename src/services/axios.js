b3623c2b1e66bbb192b94fd12dadd0465776c700
luis tormes <luisangeltormes@gmail.com>
2022-07-14 07:11:25 -0400

    coocando la url de deploy

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instanceAxios = axios.create({
   // baseURL: "http://localhost:4000/api/",
-  baseURL: "https://api-nacho-store.vercel.app/api/",
+  baseURL: "https://api-nacho-store.herokuapp.com/api/",
 });
 
 export default instanceAxios;
